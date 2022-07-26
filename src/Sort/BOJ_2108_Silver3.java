0e5679bb9b4e32297caed4ddd81ef32eb495a7b0
BaekSeungHoon <psh104404@naver.com>
2022-07-26 21:54:49 +0900

    add to sort category

--- src/Sort/BOJ_2108_Silver3.java
+++ src/Sort/BOJ_2108_Silver3.java
@@ -1,4 +1,7 @@
-import java.io.*;
+package Sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
@@ -14,7 +17,7 @@ import java.util.*;
  *
  *
  */
-class Main {
+class BOJ_2108_Silver3 {
 
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
