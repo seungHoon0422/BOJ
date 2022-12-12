8e83a2bbf5df5b955c87eb8a9c262519000d9221
seunghoon0422 <psh104404@naver.com>
2022-12-13 02:32:33 +0900

    Add : BaekJoon Alogrithm Solves

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,19 @@ public class Main {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    private static int[][] pay;
+
     public static void main(String[] args) throws IOException {
 
         int N = Integer.parseInt(br.readLine());
-        String[] inputBuilding = br.readLine().split(" ");
-        int[] building = new int[N];
-        int[] visible = new int[N];
+        pay = new int[N][N];
+        for(int i=0; i<N; i++) {
+            char[] inputLine = br.readLine().toCharArray();
+            for(int j=0; j<N; j++)
+                pay[i][j] = inputLine[j] - '0';
 
-        for (int i = 0; i < N; i++) {
-            building[i] = Integer.parseInt(inputBuilding[i]);
+
         }
-        for(int i=0; i<N-1; i++) {
-
-            // 바로 옆 아파트는 보이므로 1개 카운트
-            visible[i] += 1;
-            visible[i+1] += 1;
-
-            double slope = building[i+1] - building[i];
-            for(int j=i+2; j<N; j++) {
-                double nextslope = (double) (building[j] - building[i]) / (j-i);
-                if(nextslope <= slope) continue;
-                slope = nextslope;
-                visible[i]++;
-                visible[j]++;
-            }
-        }
-        Arrays.sort(visible);
-        System.out.println(visible[N-1]);
-        
 
 
 
@@ -44,4 +29,6 @@ public class Main {
 
 
     }
+
+
 }
