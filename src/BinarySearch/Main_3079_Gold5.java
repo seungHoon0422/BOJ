0583d5d8c27e5f0691eb0607517218be7935cea0
seunghoon0422 <psh104404@naver.com>
2022-12-14 19:51:53 +0900

    Add: 입국심사, Binary Search **

--- src/BinarySearch/Main_3079_Gold5.java
+++ src/BinarySearch/Main_3079_Gold5.java
@@ -1,10 +1,12 @@
+package BinarySearch;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_3079_Gold5 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
