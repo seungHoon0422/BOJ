6a1ede989dd3ddcc67caaecb72b00ff7b09dcec3
seunghoon0422 <psh104404@naver.com>
2022-12-14 17:27:28 +0900

    Add: 암기왕, Binary Search, sort

--- src/BinarySearch/Main_2776_Silver4.java
+++ src/BinarySearch/Main_2776_Silver4.java
@@ -1,10 +1,12 @@
+package BinarySearch;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_2776_Silver4 {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
