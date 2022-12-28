d1fa52c9d48520275a2913fd7c450ab7dd4b7b30
seunghoon0422 <psh104404@naver.com>
2022-12-28 14:02:28 +0900

    Add : 11404 플로이드, Floyd Washall

--- src/graph/Main_11404_Gold4.java
+++ src/graph/Main_11404_Gold4.java
@@ -1,10 +1,11 @@
+package graph;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_11404_Gold4 {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
