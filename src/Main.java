721db3537f1b122e5d34ed75195143cf17f6b974
seunghoon0422 <psh104404@naver.com>
2022-12-30 12:03:03 +0900

    Add : 14442 벽 부수고 이동하기 2, Bruteforce, Graph, BFS

--- src/Main.java
+++ src/Main.java
@@ -1,116 +1,104 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
 
-    private static int R, C, year;
-    private static int[][] map;
-    private static boolean[][] visited;
-    private static boolean allMelt;
+    private static char[][] map;
+    private static int[][][] visited;
+    private static int R, C, K;
+    private static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
 
         st = new StringTokenizer(br.readLine(), " ");
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
-        year = 0;
-        allMelt = false;
+        K = Integer.parseInt(st.nextToken());
+
+        visited = new int[R][C][K+1];
+        map = new char[R][C];
+        for(int i=0; i<R; i++)
+            for (int j = 0; j < C; j++)
+                for (int k = 0; k <= K; k++)
+                    visited[i][j][k] = INF;
+
 
         for(int i=0; i<R; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<C; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            map[i] = br.readLine().toCharArray();
         }
 
+        System.out.println(bfs(0,0));
 
-        while(true) {
-            if(checkSperate()) {
-                break;
-            } else if(allMelt) {
-                year = 0;
-                break;
-            }
-            melting();
-            year++;
-        }
-
-        System.out.println(year);
-    }
-
-    private static void melting() {
-        int[][] nextIce = new int[R][C];
-        for(int i=0; i<R; i++) {
-            for(int j=0; j<C; j++) {
-                if(map[i][j] == 0) continue;
-                nextIce[i][j] = map[i][j];
-                for(int d=0; d<4; d++) {
-                    int nr = i + dr[d];
-                    int nc = j + dc[d];
-                    if(nr<0 || nr>=R || nc<0 || nc>=C) continue;
-                    if(map[nr][nc] == 0) nextIce[i][j]--;
-                    if(nextIce[i][j] == 0) break;
-                }
-            }
-        }
-        map = nextIce;
-    }
-
-    private static boolean checkSperate() {
-
-        visited = new boolean[R][C];
-        allMelt = true;
-        int part = 0;
-        for(int i=0; i<R; i++) {
-            for(int j=0; j<C; j++) {
-                if(map[i][j] > 0 && !visited[i][j]) {
-                    allMelt = false;
-                    if(part == 1)
-                        return true;
-                    bfs(i,j);
-                    part++;
-                }
-            }
-        }
-        return false;
     }
 
     private static final int[] dr = {1, -1, 0, 0};
     private static final int[] dc = {0, 0, 1, -1};
 
-    private static void bfs(int r, int c) {
+    private static int bfs(int r, int c) {
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{r, c});
-        visited[r][c] = true;
-
+        int answer = -1;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(r, c, 0, 0));
+        visited[r][c][0] = 0;
         while (!queue.isEmpty()) {
-            int[] front = queue.poll();
-            r = front[0];
-            c = front[1];
+            Node node = queue.poll();
+
+            // 최종 도착지에 도달 시 다음 요소 진행
+            if(node.r == R-1 && node.c == C-1) {
+                return node.distance + 1;
+/*
+                if(answer == -1) answer = node.distance;
+                else answer = Math.min(answer, node.distance);
+                continue;
+*/
+            }
 
             for(int i=0; i<4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nr = node.r + dr[i];
+                int nc = node.c + dc[i];
 
                 if(nr<0 || nr>=R || nc<0 || nc>=C) continue;
-                if(visited[nr][nc] || map[nr][nc] == 0) continue;
 
-                visited[nr][nc] = true;
-                queue.offer(new int[]{nr, nc});
+                // 벽이 아닌 경우
+                if(map[nr][nc] == '0') {
+                    if(visited[nr][nc][node.k] > visited[node.r][node.c][node.k] + 1) {
+                        visited[nr][nc][node.k] = visited[node.r][node.c][node.k] + 1;
+                        queue.offer(new Node(nr,nc,node.k,visited[nr][nc][node.k]));
+                    }
+                } else if(map[nr][nc] == '1') {
+                    if(node.k < K) {
+                        if(visited[nr][nc][node.k + 1] > visited[node.r][node.c][node.k] + 1) {
+                            visited[nr][nc][node.k + 1] = visited[node.r][node.c][node.k] + 1;
+                            queue.offer(new Node(nr,nc,node.k+1,visited[nr][nc][node.k + 1]));
+                        }
+
+                    }
+                }
 
             }
         }
-
+        if(answer != -1) answer++;
+        return answer;
     }
 
+
+    static class Node {
+        int r, c, k;
+        int distance;
+
+        public Node(int r, int c, int k, int distance) {
+            this.r = r;
+            this.c = c;
+            this.k = k;
+            this.distance = distance;
+        }
+    }
 
 }
