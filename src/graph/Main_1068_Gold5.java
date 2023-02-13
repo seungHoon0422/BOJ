5dd0c0d78807696bed6b8b47290bd3b0618b9754
seunghoon0422 <psh104404@naver.com>
2023-02-13 16:06:29 +0900

    Add : 1068 트리 graph dfs

--- src/graph/Main_1068_Gold5.java
+++ src/graph/Main_1068_Gold5.java
@@ -1,10 +1,14 @@
+package graph;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 
-public class Main {
+public class Main_1068_Gold5 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
