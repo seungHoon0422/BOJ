384a5f3092d80b8e869b88fe7e21c82c67b06e35
seunghoon0422 <psh104404@naver.com>
2023-05-10 05:48:59 +0900

    Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,153 +1,234 @@
-import javax.lang.model.SourceVersion;
 import java.util.*;
 import java.io.*;
 
+
 /*
-    2683 치즈 Gold3
-    시작 12:20
-    종료
-    소요시간
-    * 문제 조건
-    1. 가장자리에 있는 치즈는 1시간이후에 녹는다.
-    2. 치즈로 감싸여있는 공간은 외부로 판단하지 않는다.
-    3. 치즈가 다 녹아 사라지는 시간을 구한다.
+16235 Gold3 나무제태크
+시작 : 4:31
+종료 :
+소요시간 :
 
 
-    * 문제 해결법
-    - 치즈가 녹을 때마다 사이 공간을 확인해야한다.
-    - 1시간이 지날 때마다 외부 공기와 내부공기 체크
-    - 치즈마다 2면이상 붙어있는지 확인한다.
-    - 내부공기와 붙어있는 면의 개수를 체크한다.
-    - 치즈를 녹인다.
+가장 처음에 모든 양분은 5만큼 있다.
+M 개의 나무를 구입해 나무를 심는다. -> 한칸에 여러개의 나무를 심을수 있다.
+1년은 봄, 여름, 가을, 겨울로 구성
+- 봄
+나무가 자신의 나이만큼 양분을 먹고 나이가 1 증가
+자기칸의 양분만 먹을 수 있고, 여러개의 나무가 있다면 나이가 어린 나무부터 양분을 먹는다.
+땅에 양분이 부족해 자신의 나이만큼 양분을 먹을 수 없는 나무는 양분을 먹지 못하고 즉시 죽는다.
+- 여름
+봄에 죽은 나무가 양분으로 변하게 된다.
+각각의 죽은 나무마다 나이를 2로 나눈 값이 나무가 있던 칸에 양분으로 추가된다.
+- 가을
+나무가 번식
+번식하는 나무는 나이가 5의 배수
+인접한 8개의 칸에 나이가 1인 나무가 생긴다.
+- 겨울
+땅에 양분을 추가
+각 칸에 추가되는 양분의 양은 A[r][c]이고, 입력으로 주어진다.
+
 
  */
-
-
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
+    static int[][] A, plus;
+    static ArrayList<Integer>[][] trees, deadTrees;
+    static int n, m, k;
 
 
-    static int r, c, time;
-    static int[][] board;
-    static boolean[][] inside;
-    static ArrayList<int[]> cheese;
 
     public static void main(String[] args) throws IOException {
 
         st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        board = new int[r][c];
-        time = 0;
-        cheese = new ArrayList<>();
+        A = new int[n+1][n+1];
+        plus = new int[n+1][n+1];
 
-        for(int i=0; i<r; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<c; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j] == 1) cheese.add(new int[]{i,j});
+        trees = new ArrayList[n+1][n+1];
+        deadTrees = new ArrayList[n+1][n+1];
+
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= n; j++) {
+                trees[i][j] = new ArrayList<>();
+                deadTrees[i][j] = new ArrayList<>();
+                A[i][j] = 5;
+            }
+
+        for(int i=1; i<=n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=1; j<=n; j++) {
+                plus[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-        while(!cheeseIsEmpty()) {
-            inside = new boolean[r][c];
-            checkInside();
-
-            // 사라질 치즈
-            ArrayList<int[]> byeCheese = new ArrayList<>();
-            for (int[] pos : cheese) {
-
-                int count = 0;
-                // 4방향 확인
-                for(int d=0; d<4; d++) {
-                    int nr = pos[0] + dr[d];
-                    int nc = pos[1] + dc[d];
-                    if(nr<0 || nr>=r || nc<0 || nc>=c) continue;
-                    // 내부공기가 아닌 공기와 맞닿아있는 경우
-                    if(board[nr][nc] == 0 && !inside[nr][nc]) count++;
-                } // end of d
-                if(count >= 2) byeCheese.add(pos);
-
-            } // end of cheese
-
-            for (int[] pos : byeCheese) {
-
-                board[pos[0]][pos[1]] = 0;
-                cheese.remove(pos);
-            }
-            time++;
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int age = Integer.parseInt(st.nextToken());
+            trees[r][c].add(age);
         }
 
+        while(k-- > 0) {
 
-        sb.append(time);
-        System.out.println(sb.toString());
-
-    } // end of main
-
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
-
-    private static void checkInside() {
+//            - 봄
+            spirngAction();
+//            나무가 자신의 나이만큼 양분을 먹고 나이가 1 증가
+//            자기칸의 양분만 먹을 수 있고, 여러개의 나무가 있다면 나이가 어린 나무부터 양분을 먹는다.
+//            땅에 양분이 부족해 자신의 나이만큼 양분을 먹을 수 없는 나무는 양분을 먹지 못하고 즉시 죽는다.
 
 
-        boolean[][] visited = new boolean[r][c];
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                // 치즈가 있으면 통과
-                if(board[i][j] == 1) continue;
-                if(visited[i][j]) continue;
+//            - 여름
+            summerAction();
+//            봄에 죽은 나무가 양분으로 변하게 된다.
+//            각각의 죽은 나무마다 나이를 2로 나눈 값이 나무가 있던 칸에 양분으로 추가된다.
 
-                Queue<int[]> queue = new LinkedList<>();
-                ArrayList<int[]> space = new ArrayList<>();
-                space.add(new int[]{i,j});
-                queue.offer(new int[]{i,j});
-                visited[i][j] = true;
 
-                while (!queue.isEmpty()) {
+//            - 가을
+            autumnAction();
+//            나무가 번식
+//            번식하는 나무는 나이가 5의 배수
+//            인접한 8개의 칸에 나이가 1인 나무가 생긴다.
 
-                    int[] pos = queue.poll();
-                    for(int d=0; d<4; d++) {
-                        int nr = pos[0] + dr[d];
-                        int nc = pos[1] + dc[d];
 
-                        if(nr<0 || nr>=r || nc<0 || nc>=c) continue;
-                        if(board[nr][nc] == 1) continue;
-                        if(visited[nr][nc]) continue;
+//            - 겨울
+            winterAction();
+//            땅에 양분을 추가
+//            각 칸에 추가되는 양분의 양은 A[r][c]이고, 입력으로 주어진다.
 
-                        visited[nr][nc] = true;
-                        queue.offer(new int[]{nr, nc});
-                        space.add(new int[]{nr,nc});
-                    } // end of d
-                }// end of while queue
 
-                // 내부공기인지 확인한다.
-                boolean isInside = true;
-                for(int[] pos : space) {
-                    if(pos[0] == r-1 || pos[0] == 0 || pos[1] == c-1 || pos[1] == 0)
-                    {
-                        isInside = false;
-                        break;
-                    }
-                 }
 
-                if (isInside) {
-                    for(int[] pos : space)
-                        inside[pos[0]][pos[1]] = true;
+        } // end of while
+
+
+        int answer = 0;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                answer += trees[i][j].size();
+            }
+        }
+        System.out.println(answer);
+
+
+
+
+
+
+    }
+
+
+//            나무가 자신의 나이만큼 양분을 먹고 나이가 1 증가
+//            자기칸의 양분만 먹을 수 있고, 여러개의 나무가 있다면 나이가 어린 나무부터 양분을 먹는다.
+//            땅에 양분이 부족해 자신의 나이만큼 양분을 먹을 수 없는 나무는 양분을 먹지 못하고 즉시 죽는다.
+    private static void spirngAction() {
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+
+
+                // 양분이 남아있지 않은 경우
+                if(A[i][j] == 0) continue;
+                ArrayList<Integer> localTrees = trees[i][j];
+                ArrayList<Integer> liveTrees = new ArrayList<>();
+
+
+                localTrees.sort((o1, o2) -> o1 - o2);
+                int index = 0;
+                for(; index < localTrees.size(); index++) {
+                    if(A[i][j] >= localTrees.get(index)) {
+                        A[i][j] -= localTrees.get(index);
+                        liveTrees.add(localTrees.get(index) + 1);
+                    } else break;
                 }
-            } // end of c
-        } // end of r
+
+                deadTrees[i][j].clear();
+                if (index < localTrees.size()) {
+                    for(; index < localTrees.size(); index++) {
+                        deadTrees[i][j].add(localTrees.get(index));
+                    }
+                }
+
+                trees[i][j] = liveTrees;
+
+
+            } // end of for j
+        } // end of for i
+
+
+
 
 
     }
 
-    private static boolean cheeseIsEmpty() {
-        return cheese.size() == 0 ? true : false;
+
+//            봄에 죽은 나무가 양분으로 변하게 된다.
+//            각각의 죽은 나무마다 나이를 2로 나눈 값이 나무가 있던 칸에 양분으로 추가된다.
+    private static void summerAction() {
+        // clear deadTree
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++) {
+                for (Integer deadtree : deadTrees[i][j]) {
+                    A[i][j] += deadtree / 2;
+                }
+                deadTrees[i][j].clear();
+            }
+
     }
+
+    static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dc = {0, 1, 1, 1, 0 ,-1, -1, -1};
+
+//            나무가 번식
+//            번식하는 나무는 나이가 5의 배수
+//            인접한 8개의 칸에 나이가 1인 나무가 생긴다.
+    private static void autumnAction() {
+
+        int[][] babyTrees = new int[n+1][n+1];
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                for(int k=0; k<trees[i][j].size(); k++) {
+                    int age = trees[i][j].get(k);
+                    if(age % 5 != 0) continue;
+
+                    // 나이가 5의 배수인 나무 번식 시작
+                    for(int d=0; d<dr.length; d++) {
+
+                        int nr = i + dr[d];
+                        int nc = j + dc[d];
+                        if(nr<=0 || nr>n+1 || nc<=0 || nc>n+1) continue;
+                        babyTrees[nr][nc]++;
+
+                    } // end of for dir
+                } // end of for trees
+            } // end of for j
+        } // end of for i
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                while (babyTrees[i][j]-- > 0) {
+                    trees[i][j].add(1);
+                }
+            }
+        }
+    }
+
+//            땅에 양분을 추가
+//            각 칸에 추가되는 양분의 양은 A[r][c]이고, 입력으로 주어진다.
+    private static void winterAction() {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                A[i][j] += plus[i][j];
+            }
+        }
+    }
+
 
 
 }
