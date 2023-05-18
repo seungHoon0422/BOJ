ac1e97290bfe1f8f202b012d8f1362e8b3400865
seunghoon0422 <psh104404@naver.com>
2023-05-18 16:29:02 +0900

    * 평범한 배낭 : knapsack, dp, bottom-up

--- src/DP/Main_12865_Gold5_2.java
+++ src/DP/Main_12865_Gold5_2.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 /*
@@ -12,7 +16,7 @@ import java.io.*;
 
  */
 
-public class Main {
+public class Main_12865_Gold5_2 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
